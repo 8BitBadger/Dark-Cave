88794ff457e9873395c900ddba344eabd7109471
Gerhardt <littlebear101@gmail.com>
2018-05-16 21:56:36 +0200

    Various updates
    Working on mouse cursor, mining and attack distance, interval and drops that need to happen

---
+++ Dark Cave/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public ActorStats stats;
+
+    //The Rigidbody2D for the Actor
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        stats.speedModifier = 1f;
+
+        if (gameObject.GetComponent<Rigidbody2D>())
+        {
+            rb2d = gameObject.GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+            //Set up the rigibody2d component for the actor
+            //SetUpRigidBody();
+        }
+
+        //animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Do take damage animation
+        stats.health -= damage;
+
+        if(stats.health <= 0)
+        {
+
+        }
+    }
+
+    void Die()
+    {
+        //Play death animation
+        //Drop crystal
+        Destroy(gameObject);
+    }
+
+    void DropCrystal()
+    {
+        //TODO: detirmine the crystel worth according to the monster stats
+    }
+
+}
--- Dark Cave/Assets/Scripts/InputManager.cs
+++ Dark Cave/Assets/Scripts/InputManager.cs
@@ -6,6 +6,9 @@ public class InputManager : MonoBehaviour
 {
     public GameObject cursorPrefab;
 
+    public ActorStats playerStats;
+    private Player playerInstance;
+
     //The world coordinates of the mouse in the last frame
     Vector3 lastFramePosition;
     Vector3 currFramePosition;
@@ -13,9 +16,18 @@ public class InputManager : MonoBehaviour
     bool buildModeIsObjects = false;
     TileType buildModeTile = TileType.Floor;
 
+    float timeSinceLastAttack = 0f;
+
+    private void Start()
+    {
+        playerInstance = GameObject.FindGameObjectWithTag("Player").GetComponent("Player") as Player;
+        cursorPrefab = Instantiate(cursorPrefab);
+        Cursor.visible = false;
+    }
 
     private void Update()
     {
+        cursorPrefab.transform.position = GetMousePosition();
 
         //TODO:Impliment build mode later in the game this is the camera movement for it
         //currFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -26,21 +38,48 @@ public class InputManager : MonoBehaviour
         //lastFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //lastFramePosition.z = 0;
 
+
+
         if (Input.GetMouseButton(0))
         {
-            Tile t = WorldManager.Instance.GetTileAtWorldCoord(GetMousePosForTile());
-
-            if (t != null && t.Type != TileType.Rock && t.Room == RoomType.None)
+            if ((Time.time - timeSinceLastAttack) > playerStats.attackInterval)
             {
-                t.Type = buildModeTile;
+                RaycastHit2D[] hit = Physics2D.LinecastAll(GetMousePosition2D(), GetMousePosition2D());
+
+                Tile tile = WorldManager.Instance.GetTileAtWorldCoord(GetMousePosForTile());
+                if (Vector2.Distance(new Vector2(tile.X, tile.Y), playerInstance.transform.position) < 1)
+                {
+                    if (tile != null && tile.Type != TileType.Rock && tile.Room == RoomType.None)
+                    {
+                        tile.Type = buildModeTile;
+                    }
+                }
+
+                for (int i = 0; i < hit.Length; i++)
+                {
+                    if (hit[i].collider.tag == "")
+                    {
+
+                    }
+                }
+
+                timeSinceLastAttack = Time.time;
             }
+
+
         }
     }
 
     Vector3 GetMousePosition()
     {
-        
+
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    Vector2 GetMousePosition2D()
+    {
+
+        return new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
     }
 
     Vector3 GetMousePosForTile()
@@ -57,9 +96,6 @@ public class InputManager : MonoBehaviour
         buildModeIsObjects = false;
         buildModeTile = TileType.Floor;
     }
-
-
-
 
     void UpdateCameraMovement()
     {
